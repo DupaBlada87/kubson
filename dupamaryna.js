82bd33195f9ffc4ac964a12bafe771ec5579bb23
camel <ccamell@o2.pl>
2022-11-28 18:39:18 +0100

    v1.3

--- dupamaryna.js
+++ dupamaryna.js
@@ -18,8 +18,9 @@ var wait=1000
 var checkSSJ=true;
 var czas=0;
 let soul_fight = true;
-let what_to_train = 0;
+let what_to_train = 1;
 let code_bool = true;
+let what_to_traintime = 1;
 
 $(document).bind('keydown', '1', function(){
         if(JQS.chm.is(":focus") == false){
@@ -27,13 +28,14 @@ $(document).bind('keydown', '1', function(){
         }
         return false;
     });
-const $css = "#gh_game_helper {min-width:130px; padding:5px; border:solid gray 1px; background:rgba(22, 22, 93, 0.81); color:gold; position: fixed; top: 40px; right: 5px; z-index:5;}#gh_game_helper .gh_button {cursor:pointer;text-align:center; border-bottom:solid gray 1px;}";
+const $css = "#gh_game_helper {min-width:150px; padding:5px; border:solid gray 1px; background:rgba(22, 22, 93, 0.81); color:gold; position: fixed; top: 40px; right: 5px; z-index:5;}#gh_game_helper .gh_button {cursor:pointer;text-align:center; border-bottom:solid gray 1px;}";
 
 const $html = "<div class='gh_button gh_pvp'>Kody <b class='gh_status red'>Off</b></div>";
 const $html2 = "<div id='soulfight' class='gh_button'>Otchłań <b class='gh_status green'>On</b></div>";
 const $html3 = "<label class='select_input'><select id='bot_what_to_train'><option value='1'>Siła</option><option value='2'>Szybkość</option><option value='3'>Wytrzymałość</option><option value='4'>Siła Woli</option><option value='5'>Energia Ki</option><option value='6'>Wtajemniczenie</option></select></label>";
+const $html4 = "<label class='select_input'><select id='bot_what_to_traintime'><option value='1'>1 godz.</option><option value='2'>2 godz.</option><option value='3'>3 godz.</option><option value='4'>4 godz.</option><option value='5'>5 godz.</option><option value='6'>6 godz.</option><option value='7'>7 godz.</option><option value='8'>8 godz.</option><option value='9'>9 godz.</option><option value='10'>10 godz.</option><option value='11'>11 godz.</option><option value='12'>12 godz.</option></select></label>";
 
-$('body').append("<div id='gh_game_helper'>"+$html+$html2+$html3+"</div>").append("<style>"+$css+"</style>");
+$('body').append("<div id='gh_game_helper'>"+$html+$html2+$html3+$html4+"</div>").append("<style>"+$css+"</style>");
 
 
 $('#gh_game_helper .gh_pvp').click(() => {
@@ -54,6 +56,9 @@ $('#bot_what_to_train').change((e) => {
 	what_to_train = parseInt($(e.target).val());
 });
 
+$('#bot_what_to_traintime').change((e) => {
+	what_to_traintime = parseInt($(e.target).val());
+});
 $('#soulfight').click(() => {
 	if(soul_fight) {
 		$("#soulfight .gh_status").removeClass("green").addClass("red").html("Off");
@@ -149,7 +154,7 @@ function tren(){
 	if(GAME.is_training){
 		window.setTimeout(start,wait);
 	} else {
-		GAME.socket.emit('ga',{a:8,type:2,stat:what_to_train,duration:12,code:kody});
+		GAME.socket.emit('ga',{a:8,type:2,stat:what_to_train,duration:what_to_traintime,code:kody});
 		window.setTimeout(start,wait);
 	}
 }
